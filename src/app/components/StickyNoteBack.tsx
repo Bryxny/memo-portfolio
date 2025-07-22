6fe97fe8567015a393d99b839f4e954b7b98dc1a
Bryxny <bryonydoh2@gmail.com>
2025-07-22 19:14:57 +0100

    feat: added projects

--- src/app/components/StickyNoteBack.tsx
+++ src/app/components/StickyNoteBack.tsx
@@ -23,26 +23,26 @@ export default function StickyNoteBack({
       document.body.style.overflow = "";
     };
   }, []);
-
+  console.log(img);
   return (
     <div className="fixed inset-0 z-[100] bg-black/30 flex items-start justify-center overflow-auto pt-10 pb-20">
       <div
-        className="bg-white w-[800px] min-h-[800px] max-w-full p-6 shadow-lg border border-gray-300 rounded-xl overflow-auto"
+        className="bg-white w-[800px] min-h-[800px] max-w-full p-10 shadow-lg border border-gray-300 rounded-l overflow-auto"
         style={{ backgroundColor: "#efecec" }}
       >
         <p className="text-2xl font-bold">{title}</p>
-        <img src={img} className="w-full max-w-md my-4" />
+        <img src={img} className="w-full my-4" />
         <p className="mb-4">{longdescription}</p>
 
         <ul className="mb-4 list-disc list-inside">
-          <li className="font-semibold">Features:</li>
+          <li className="font-semibold list-none">Features:</li>
           {features.map((feature) => (
             <li key={feature}>{feature}</li>
           ))}
         </ul>
 
         <ul className="mb-4 list-disc list-inside">
-          <li className="font-semibold">Techstack:</li>
+          <li className="font-semibold list-none">Techstack:</li>
           {techStack?.map((tech) => (
             <li key={tech}>{tech}</li>
           ))}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import StickyGrid from "./components/StickyGrid";
 export default function Home() {
   return (
     <div
-      className="relative mx-auto w-full bg-[url('/Background.webp')] bg-cover overflow-y-auto overflow-x-hidden"
+      className="relative mx-auto w-full bg-[url('/Background.webp')] bg-cover overflow-y-auto"
       style={{ height: "1300px", touchAction: "none" }}
     >
       <StickyGrid />
